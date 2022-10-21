5b90b0ea19651f80a67c403d11285e23163add3a
Shane O'G <G00411304@gmit.ie>
2022-10-21 13:57:21 +0100

    Assignment completed

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,25 +1,26 @@
 # Write a program in python that will read a file from a repository, 
 # The program should then replace all the instances of the text "Andrew" with your name
 # The program should then commit those changes and push the file back to the repository.
-# I do not need to see your keys
-# (see lab2, to follow)
-# Handup: Save the program as assignment04-github.py to the same repository you uploaded the xml to
-# Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
 
 from github import Github 
 from config import config
-import requests
 
+# Get github key from config
 gitHubKey = config['gitHubKey']
 g = Github(gitHubKey)
 
+# Get my repo
 repo = g.get_repo("ShaneOG2/data-representation-coursework")
 
+# Get the contents of assignment04-file.txt
 fileContent = repo.get_contents("assignment04-file.txt")
 
+# Decode the contents
 fileContentDecoded = fileContent.decoded_content.decode() # https://stackoverflow.com/a/64739176
 
+# Convert text to lower and replace andrew with Shane
 fileContentDecoded = fileContentDecoded.lower()
 fileContentDecoded = fileContentDecoded.replace("andrew", "Shane")
 
-print(fileContentDecoded)
+# Update and commit to github 
+gitHubResponse = repo.update_file(fileContent.path, "Updated by programme", fileContentDecoded, fileContent.sha)
