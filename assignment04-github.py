67e4811aeecb3bfb184b0238c3eefd5922aaf142
Shane O'G <G00411304@gmit.ie>
2022-10-21 14:02:04 +0100

    Assignment completed

--- assignment04-github.py
+++ assignment04-github.py
@@ -2,25 +2,25 @@
 # The program should then replace all the instances of the text "Andrew" with your name
 # The program should then commit those changes and push the file back to the repository.
 
-#from github import Github 
-#from config import config
+from github import Github 
+from config import config
 
 # Get github key from config
-#gitHubKey = config['gitHubKey']
-#g = Github(gitHubKey)
+gitHubKey = config['gitHubKey']
+g = Github(gitHubKey)
 
 # Get my repo
-#repo = g.get_repo("ShaneOG2/data-representation-coursework")
+repo = g.get_repo("ShaneOG2/data-representation-coursework")
 
 # Get the contents of assignment04-file.txt
-#fileContent = repo.get_contents("assignment04-file.txt")
+fileContent = repo.get_contents("assignment04-file.txt")
 
 # Decode the contents
-#fileContentDecoded = fileContent.decoded_content.decode() # https://stackoverflow.com/a/64739176
+fileContentDecoded = fileContent.decoded_content.decode() # https://stackoverflow.com/a/64739176
 
 # Convert text to lower and replace andrew with Shane
-#fileContentDecoded = fileContentDecoded.lower()
-#fileContentDecoded = fileContentDecoded.replace("andrew", "Shane")
+fileContentDecoded = fileContentDecoded.lower()
+fileContentDecoded = fileContentDecoded.replace("andrew", "Shane")
 
 # Update and commit to github 
-#gitHubResponse = repo.update_file(fileContent.path, "Updated by programme", fileContentDecoded, fileContent.sha)
+gitHubResponse = repo.update_file(fileContent.path, "Updated by programme", fileContentDecoded, fileContent.sha)
