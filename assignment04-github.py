d8deade929d211cc6dbc92025ee1eb65d372024d
Shane O'G <G00411304@gmit.ie>
2022-10-21 12:55:01 +0100

    Config added to gitignore

---
+++ assignment04-github.py
@@ -0,0 +1,22 @@
+# Write a program in python that will read a file from a repository, 
+# The program should then replace all the instances of the text "Andrew" with your name
+# The program should then commit those changes and push the file back to the repository.
+# I do not need to see your keys
+# (see lab2, to follow)
+# Handup: Save the program as assignment04-github.py to the same repository you uploaded the xml to
+# Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
+
+from github import Github 
+from config import config
+import requests
+
+gitHubKey = config['gitHubKey']
+g = Github(gitHubKey)
+
+repo = g.get_repo("ShaneOG2/data-representation-coursework")
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+#print(urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
